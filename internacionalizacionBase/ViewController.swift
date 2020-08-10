54ef120e432d3750c6136cf045fcf411e63a8cb6
Arlen <arlen.pena@gmail.com>
2020-08-09 19:12:49 -0500

    Initial Commit

---
+++ internacionalizacionBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  internacionalizacionBase
+//
+//  Created by Arlen Peña on 09/08/20.
+//  Copyright © 2020 Arlen Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
